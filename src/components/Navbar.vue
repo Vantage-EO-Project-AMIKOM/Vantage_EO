35d1671502fa3ed956d7444a26422e97ce80479b
Angga <putraa.bar9@gmail.com>
2026-05-17 18:37:19 +0700

    benerin warna navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -18,12 +18,12 @@ const isOpen = ref(false)
         </RouterLink>
 
         <div class="hidden xl:flex items-center space-x-6">
-            <RouterLink to="/" class="transition-all hover:text-[#788663]">Home</RouterLink>
-            <RouterLink to="about" class="transition-all hover:text-[#788663]">About Us</RouterLink>
-            <RouterLink to="#" class="transition-all hover:text-[#788663]">Events</RouterLink>
-            <RouterLink to="profile" class="transition-all hover:text-[#788663]">Profile</RouterLink>
-            <RouterLink to="ourHistory" class="transition-all hover:text-[#788663]">Our Story</RouterLink>
-            <RouterLink to="contact" class="transition-all hover:text-[#788663]">Contact Us</RouterLink>
+            <RouterLink to="/" class="transition-all hover:text-blue-800">Home</RouterLink>
+            <RouterLink to="about" class="transition-all hover:text-blue-800">About Us</RouterLink>
+            <RouterLink to="#" class="transition-all hover:text-blue-800">Events</RouterLink>
+            <RouterLink to="profile" class="transition-all hover:text-blue-800">Profile</RouterLink>
+            <RouterLink to="ourHistory" class="transition-all hover:text-blue-800">Our Story</RouterLink>
+            <RouterLink to="contact" class="transition-all hover:text-blue-800">Contact Us</RouterLink>
         </div>
 
         <div class="hidden xl:flex justify-center items-center space-x-4 text-black">
@@ -34,7 +34,7 @@ const isOpen = ref(false)
                     <i class="fa fa-search" aria-hidden="true"></i>
                 </button>
             </div>
-            <button class="bg-[#17202A] text-white px-5 py-2 rounded-full transition-all hover:bg-[#788700]">Register
+            <button class="bg-[#17202A] text-white px-5 py-2 rounded-full transition-all hover:bg-[#425c79]">Register
                 <i class="fa fa-long-arrow-right font-extralight -rotate-45" aria-hidden="true"></i>
             </button>
         </div>
@@ -45,7 +45,7 @@ const isOpen = ref(false)
     </nav>
 
     <div v-if="isOpen"
-        class="fixed top-28 left-1/2 -translate-x-1/2 w-[95%] bg-[#FFF5DD] shadow-2xl rounded-2xl p-6 flex flex-col space-y-4 z-40 xl:hidden">
+        class="fixed top-28 left-1/2 -translate-x-1/2 w-[95%] bg-[#17202A] text-white shadow-2xl rounded-2xl p-6 flex flex-col space-y-4 z-40 xl:hidden">
         <RouterLink to="/" class=" text-lg hover:text-blue-800">Home</RouterLink>
         <RouterLink to="about" class=" text-lg hover:text-blue-800">Pages</RouterLink>
         <RouterLink to="#" class=" text-lg hover:text-blue-800">Events</RouterLink>
@@ -57,10 +57,10 @@ const isOpen = ref(false)
             <input type="text" placeholder="Search..."
                 class="w-full bg-white pl-5 pr-2 py-3 rounded-l-full outline-none border border-gray-200">
             <button class="bg-white pr-5 py-3 rounded-r-full border border-l-0 border-gray-200 hover:bg-gray-100">
-                <i class="fa fa-search" aria-hidden="true"></i>
+                <i class="fa fa-search text-black" aria-hidden="true"></i>
             </button>
         </div>
         <button
-            class="w-full bg-[#788663] text-white font-bold px-5 py-3 mt-2 rounded-full transition-all hover:bg-[#788700]">Register</button>
+            class="w-full bg-[#FF0000] text-white font-bold px-5 py-3 mt-2 rounded-full transition-all hover:bg-[#ac2c2c]">Register</button>
     </div>
 </template>
