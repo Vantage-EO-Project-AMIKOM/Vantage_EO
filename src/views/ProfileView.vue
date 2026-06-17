fd517e9eca9d9c5df4c238820bd8ab9a06ad367a
Puan <puanaeesya@students.amikom.ac.id>
2026-06-17 23:06:11 +0700

    responsiveee

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -31,45 +31,35 @@ onUnmounted(() => {
 </script>
 <template>
   <!-- HERO -->
-  <section
-  class="w-full min-h-[70vh] md:min-h-[85vh] flex items-center relative overflow-hidden">
-  >
+  <section class="w-full h-130 z-10 flex items-center relative overflow-hidden">
     <div class="absolute inset-0 bg-black/60">
       <img src="./../components/img/bg/hero-bg.jpg" alt=""
         class="w-full h-full object-cover brightness-[0.4] saturate-50 absolute top-0 left-0 opacity-100 bg-size-cover"
         draggable="false" :style="{ transform: `translateY(${parallaxOffset}px)` }" />
     </div>
     <div class="w-full max-w-3/4 mx-auto z-10">
-      <h1
-        class="w-full md:w-2/3 capitalize
-        text-4xl sm:text-5xl md:text-6xl lg:text-7xl
-        font-bold text-white leading-tight"
-      >
+      <h1 class="w-2/3 capitalize text-7xl font-bold text-white text-shadow-lg/50 text-shadow-black mb-5">
         Profile
       </h1>
-      <p class="text-sm sm:text-base md:text-lg uppercase text-white">vantage > profile</p>
+      <p class="text-lg font-normal uppercase text-white">vantage > profile</p>
     </div>
   </section>
 
   <!-- DID -->
-  <div
-    class="relative z-10 bg-[#2B3B4C]
-    py-10 sm:py-16 md:py-20
-    rounded-t-4xl md:rounded-[3rem]"
->
-    <section class="max-w-[90%] md:max-w-[87.5%] w-full mx-auto px-4">
+  <div class="relative z-10 bg-[#2B3B4C] py-20 rounded-[3rem] -mt-10 mb-250 xl:mb-162.5 shadow-[0_0_80px_rgba(0,0,0,0.15)]">
+    <section class="max-w-7/8 w-full mx-auto">
       <div class="bg-linear-to-br from-[#24364d] to-[#1d2d42] overflow-hidden p-15 rounded-[60px]">
-        <h2 class="text-3xl sm:text-4xl md:text-5xl lg:text-6xl capitalize font-semibold">
+        <h2 class="text-6xl capitalize font-semibold mb-8 text-center text-white">
           DID YOU KNOW US?
         </h2>
-        <p class="text-white text-base sm:text-lg md:text-xl lg:text-2xl">
+        <p class="text-white text-2xl indent-15 mb-8 text-justify">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis lacinia leo. Donec
           tincidunt congue augue vel ullamcorper. Mauris venenatis id ligula et viverra. Mauris
           suscipit aliquam tellus nec euismod. Vestibulum vestibulum dictum venenatis. Curabitur
           laoreet eu augue ut maximus. Curabitur a arcu enim. Mauris viverra velit euismod sem
           feugiat, ut porta est semper.
         </p>
-        <p class="text-white text-base sm:text-lg md:text-xl lg:text-2xl">
+        <p class="text-white text-2xl indent-15 text-justify">
           Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
           himenaeos. Sed efficitur nisi lorem, eget mattis massa aliquam et. Morbi ultricies justo
           ullamcorper diam semper efficitur. Nulla eget nulla nec massa imperdiet tristique et non
@@ -84,87 +74,87 @@ onUnmounted(() => {
     </section>
 
     <!-- Our Profile Section -->
-    <section class="w-full h-auto">
-    <div class="w-full max-w-4/5 mx-auto py-20">
-      <h2 class="text-6xl capitalize font-semibold text-center text-white">Our Profile</h2>
+   <section class="w-full h-auto">
+      <div class="w-full max-w-[95%] xl:max-w-4/5 mx-auto py-20">
+        <h2 class="text-4xl lg:text-6xl capitalize font-semibold text-center text-white">Our Profile</h2>
 
-      <div class="grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-10 mt-10 items-center justify-items-center">
-      
-      <div class="w-full max-w-70 sm:max-w-87.5 md:max-w-105 aspect-square overflow-hidden rounded-full shrink-0">
-        <img class="w-full h-full object-cover object-center scale-110"
-          src="./../components/img/profilePage/profile/profile1.jpeg" alt="Azka" />
-      </div>
-      
-      <div class="w-full h-full col-span-1 lg:col-span-2 border border-white/10 bg-linear-to-br from-[#24364d] to-[#1d2d42] shadow-[0_10px_40px_rgba(0,0,0,0.35)] rounded-4xl ring-1 ring-white/40 p-8 md:p-10 flex flex-col justify-center">
-        <h2 class="text-2xl sm:text-3xl md:text-4xl capitalize font-semibold mb-2 text-center lg:text-left text-white">
-          Dhiya Muhammad Azka
-        </h2>
-        <h2 class="text-2xl sm:text-3xl md:text-4xl capitalize font-semibold mb-6 text-center lg:text-left text-white">
-          24.61.0270
-        </h2>
-        <p class="text-xl md:text-2xl italic text-white mb-6 text-center lg:text-left">
-          “Tenang dalam langkah, kuat dalam tujuan, dan selalu jadi cahaya di setiap perjalanan.”
-        </p>
-        <div class="flex justify-center lg:justify-start items-center gap-3 mt-4">
-          <img src="./../components/img/profilePage/githubicon.png" alt="github_icon" class="w-8 h-8 object-contain" />
-          <h5 class="text-white text-xl md:text-2xl">MehmedAzka</h5>
-        </div>
-      </div>
-    </div>
-        <div class="grid grid-cols-3 gap-10 mt-10">
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-6 lg:gap-10 mt-10 items-center">
+          <div class="w-full max-w-[320px] lg:max-w-none aspect-110/145 overflow-hidden rounded-full mx-auto">
+            <img class="w-full h-full object-cover object-[0%_100%] scale-130 origin-center"
+              src="./../components/img/profilePage/profile/profile1.jpeg" alt="Azka" />
+          </div>
           <div
-            class="w-full h-auto col-span-2 border border-white/10 bg-linear-to-br from-[#24364d] to-[#1d2d42] shadow-[0_10px_40px_rgba(0,0,0,0.35)] overflow-hidden p-10 inset-0 rounded-4xl ring-1 ring-white/40 py-20 self-center">
-            <h2 class="text-2xl sm:text-3xl md:text-4xl capitalize font-semibold mb-2 text-right text-white">Puan Aeesya</h2>
-            <h2 class="text-2xl sm:text-3xl md:text-4xl capitalize font-semibold mb-8 text-right text-white">24.61.0274</h2>
-            <p class="text-2xl italic text-white mb-10 text-right">
+            class="w-full h-auto lg:col-span-2 border border-white/10 bg-linear-to-br from-[#24364d] to-[#1d2d42] shadow-[0_10px_40px_rgba(0,0,0,0.35)] overflow-hidden p-6 lg:p-10 rounded-4xl ring-1 ring-white/40 py-10 lg:py-20">
+            <h2 class="text-2xl lg:text-4xl capitalize font-semibold mb-2 text-left text-white">
+              Dhiya mamat Azka
+            </h2>
+            <h2 class="text-2xl lg:text-4xl capitalize font-semibold mb-8 text-left text-white">24.61.0270</h2>
+            <p class="text-lg lg:text-2xl italic text-white mb-10 text-justify">
+              “Tenang dalam langkah, kuat dalam tujuan, dan selalu jadi cahaya di setiap
+              perjalanan.”
+            </p>
+            <div class="flex justify-start items-center gap-3 mt-10">
+              <img src="./../components/img/profilePage/githubicon.png" alt="github_icon" class="w-6 h-6 lg:w-auto" />
+              <h5 class="text-white text-lg lg:text-2xl ml-3">MehmedAzka</h5>
+            </div>
+          </div>
+        </div>
+
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-6 lg:gap-10 mt-10 items-center">
+          <div
+            class="w-full h-auto lg:col-span-2 order-2 lg:order-1 border border-white/10 bg-linear-to-br from-[#24364d] to-[#1d2d42] shadow-[0_10px_40px_rgba(0,0,0,0.35)] overflow-hidden p-6 lg:p-10 rounded-4xl ring-1 ring-white/40 py-10 lg:py-20">
+            <h2 class="text-2xl lg:text-4xl capitalize font-semibold mb-2 text-left lg:text-right text-white">Puan Aeesya</h2>
+            <h2 class="text-2xl lg:text-4xl capitalize font-semibold mb-8 text-left lg:text-right text-white">24.61.0274</h2>
+            <p class="text-lg lg:text-2xl italic text-white mb-10 text-justify lg:text-right">
               “Elegan dalam sikap, berani dalam mimpi, dan tetap bersinar tanpa harus merendahkan
               siapa pun.”
             </p>
-            <div class="flex justify-end items-center gap-3 mt-10">
-              <h5 class="text-white text-2xl ml-3 self-center">syaaaa07</h5>
-              <img src="./../components/img/profilePage/githubicon.png" alt="github_icon" />
+            <div class="flex justify-start lg:justify-end items-center gap-3 mt-10">
+              <h5 class="text-white text-lg lg:text-2xl mr-3 lg:ml-3">syaaaa07</h5>
+              <img src="./../components/img/profilePage/githubicon.png" alt="github_icon" class="w-6 h-6 lg:w-auto" />
             </div>
           </div>
-          <div class="w-full h-145 overflow-hidden rounded-full">
+          <div class="w-full max-w-[320px] lg:max-w-none aspect-[110/145] overflow-hidden rounded-full mx-auto order-1 lg:order-2">
             <img class="w-full h-full object-cover object-[85%_15%] scale-150 origin-center"
               src="./../components/img/profilePage/profile/profile2.jpeg" alt="Puan" />
           </div>
         </div>
-        <div class="grid grid-cols-3 mt-10">
-          <div class="w-full max-w-70 sm:max-w-87.5 md:max-w-105 h-80 sm:h-105 md:h-140 mx-auto overflow-hidden rounded-full">
-            <img class=" w-full h-full object-cover object-[0%_100%] scale-130 origin-center"
+
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-6 lg:gap-10 mt-10 items-center">
+          <div class="w-full max-w-[320px] lg:max-w-none aspect-[110/145] overflow-hidden rounded-full mx-auto">
+            <img class="w-full h-full object-cover object-[0%_100%] scale-130 origin-center"
               src="./../components/img/profilePage/profile/profile3.jpeg" alt="Angga" />
           </div>
           <div
-            class="w-full h-auto col-span-2 border border-white/10 bg-linear-to-br from-[#24364d] to-[#1d2d42] shadow-[0_10px_40px_rgba(0,0,0,0.35)] overflow-hidden p-10 inset-0 rounded-4xl ring-1 ring-white/40 py-20 self-center">
-            <h2 class="text-2xl sm:text-3xl md:text-4xl4xl capitalize font-semibold mb-2 text-center lg:text-left text-white">
+            class="w-full h-auto lg:col-span-2 border border-white/10 bg-linear-to-br from-[#24364d] to-[#1d2d42] shadow-[0_10px_40px_rgba(0,0,0,0.35)] overflow-hidden p-6 lg:p-10 rounded-4xl ring-1 ring-white/40 py-10 lg:py-20">
+            <h2 class="text-2xl lg:text-4xl capitalize font-semibold mb-2 text-left text-white">
               L.M.Hanggara Putra Akbar
             </h2>
-            <h2 class="text-2xl sm:text-3xl md:text-4xl capitalize font-semibold mb-8 text-center lg:text-left text-white">24.61.0279</h2>
-            <p class="text-2xl italic text-white mb-10 text-justify">
+            <h2 class="text-2xl lg:text-4xl capitalize font-semibold mb-8 text-left text-white">24.61.0279</h2>
+            <p class="text-lg lg:text-2xl italic text-white mb-10 text-justify">
               “Dilahirkan untuk memimpin, tumbuh untuk menginspirasi, dan melangkah tanpa takut menghadapi dunia.”
             </p>
             <div class="flex justify-start items-center gap-3 mt-10">
-              <img src="./../components/img/profilePage/githubicon.png" alt="github_icon" />
-              <h5 class="text-white text-2xl ml-3 self-center">agashesh</h5>
+              <img src="./../components/img/profilePage/githubicon.png" alt="github_icon" class="w-6 h-6 lg:w-auto" />
+              <h5 class="text-white text-lg lg:text-2xl ml-3">agashesh</h5>
             </div>
           </div>
         </div>
 
-        <div class="grid grid-cols-1 lg:grid-cols-3 gap-8 items-center mt-10">
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-6 lg:gap-10 mt-10 items-center">
           <div
-            class="w-full h-auto col-span-2 border border-white/10 bg-linear-to-br from-[#24364d] to-[#1d2d42] shadow-[0_10px_40px_rgba(0,0,0,0.35)] overflow-hidden p-10 inset-0 rounded-4xl ring-1 ring-white/40 py-20 self-center">
-            <h2 class="text-4xl capitalize font-semibold mb-2 text-right text-white">Dani Zakhran</h2>
-            <h2 class="text-4xl capitalize font-semibold mb-8 text-right text-white">21.61.0227</h2>
-            <p class="text-2xl italic text-white mb-10 text-right">
+            class="w-full h-auto lg:col-span-2 order-2 lg:order-1 border border-white/10 bg-linear-to-br from-[#24364d] to-[#1d2d42] shadow-[0_10px_40px_rgba(0,0,0,0.35)] overflow-hidden p-6 lg:p-10 rounded-4xl ring-1 ring-white/40 py-10 lg:py-20">
+            <h2 class="text-2xl lg:text-4xl capitalize font-semibold mb-2 text-left lg:text-right text-white">Dani Zakhran</h2>
+            <h2 class="text-2xl lg:text-4xl capitalize font-semibold mb-8 text-left lg:text-right text-white">21.61.0227</h2>
+            <p class="text-lg lg:text-2xl italic text-white mb-10 text-justify lg:text-right">
               “Bukan tentang seberapa cepat melaju, tapi seberapa konsisten tetap bertahan sampai tujuan.”
             </p>
-            <div class="flex justify-center lg:justify-end items-center gap-3 mt-6 md:mt-10">
-              <h5 class="text-white text-2xl ml-3 self-center">Danizakhran</h5>
-              <img src="./../components/img/profilePage/githubicon.png" alt="github_icon" />
+            <div class="flex justify-start lg:justify-end items-center gap-3 mt-10">
+              <h5 class="text-white text-lg lg:text-2xl mr-3 lg:ml-3">Danizakhran</h5>
+              <img src="./../components/img/profilePage/githubicon.png" alt="github_icon" class="w-6 h-6 lg:w-auto" />
             </div>
           </div>
-          <div class="w-full h-145 overflow-hidden rounded-full">
+          <div class="w-full max-w-[320px] lg:max-w-none aspect-[110/145] overflow-hidden rounded-full mx-auto order-1 lg:order-2">
             <img class="w-full h-full object-cover object-[10%_-6rem] scale-150 origin-center" src="./../components/img/profilePage/profile/profile4.jpeg" alt="Dani" />
           </div>
         </div>
@@ -172,3 +162,4 @@ onUnmounted(() => {
     </section>
   </div>
 </template>
+
